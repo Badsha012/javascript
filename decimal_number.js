159b882cf3b96f6c73dc1ba3274ff7384964a9bd
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-25 22:50:12 +0600

    Add files via upload

---
+++ decimal_number.js
@@ -0,0 +1,13 @@
+var mydecimal=7.9;
+
+ var mydecimal =0.00;
+ console.log(mydecimal);
+ //multidecimal number
+ var p=4.5*7.9;
+ console. log(p);
+ //quisant
+ var quisant=30.9/13.7;
+ console.log(quisant);
+ //finding remainder
+ var remainder=66%7;
+ console .log(remainder)
