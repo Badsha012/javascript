7cbe83c913676f6532c1c441d0a7ce9b7644533e
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-25 23:02:57 +0600

    Add files via upload

---
+++ arugemened.js
@@ -0,0 +1,25 @@
+//compound assignment with arugmented
+var a=9;
+var b=8;
+var c=7;
+a=a+9;
+b=b+122;
+c=c+9;
+console.log(a,b,c);
+//compound aasignment substration with the argument 
+var x=88;
+var y=23;
+var z=99;
+x=x-33;
+y=y-9;
+z=z-87;
+console.log(x,y,z);
+//compond multiplacation asigment with the argument
+var p=9;
+var q=8;
+var r=8;
+p=p*4;
+q=q*6;
+r=r*7;
+console.log(p,q,r);
+
