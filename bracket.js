fb264ea9ad88046f9afeed04d441d1873f76da00
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-26 07:38:54 +0600

    Add files via upload

---
+++ bracket.js
@@ -0,0 +1,10 @@
+//bracket Noation to find frist character in string
+var l="";
+var f="Badsha";
+l=f[0];
+console.log(l);
+//lastname of the string
+var x="";
+var y="Bangladesh";
+x=y[6];
+console.log(x);
