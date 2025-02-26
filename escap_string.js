0d7a933754f30b3ec9992cff4309820cc37e2a6c
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-26 07:25:45 +0600

    Add files via upload

---
+++ escap_string.js
@@ -0,0 +1,25 @@
+//*****
+// code output
+// n newlinw
+// r carryretrn 
+//  */
+var myname="I am badsha sheikh."+"he is 77 year old"
+console.log(myname);
+//only change the string
+var myStr="i love javaScript"+"l love css";
+//conctating sring 
+var myour="i come frist";
+myour += "I come  second.";
+console.log(myour);
+//contraing string with the variable
+var myName="Freecode cam";
+//find the length string
+var frisrtnamelength=0;
+var fristname="Badsha";
+frisrtnamelength=fristname.length;
+var lastnamelength=0;
+var lastname="Sheikh";
+lastnamelength=lastname;
+console.log(lastnamelength);
+
+
