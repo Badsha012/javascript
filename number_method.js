60bb5af2197f248427dc87a7827c7d67311125fd
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-20 20:19:49 +0600

    Add files via upload

---
+++ number_method.js
@@ -0,0 +1,8 @@
+var num=20;
+console.log(typeof(num));
+// Output: number
+// Here, the variable num is assigned a value of 20. The typeof operator returns the data type of the variable which is a number.
+num=toString(num);
+console.log(typeof(num));
+// Output: string
+// Here, the variable num is converted to a string using the toString() method. The typeof operator returns the data type of the variable which is a string.
