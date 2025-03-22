326817ac58e610e19a8ae73491dabf1f4b777682
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-22 09:18:26 +0600

    Add files via upload

---
+++ basic3.js
@@ -0,0 +1,7 @@
+const username='Badsha Sheikh';
+const income=3752.67;
+const expence=2580.5;
+console.log('personal Budget Tracker App');
+console.log('User:'+username);
+console.log('Total  Income:'+income);
+console.log('Total Expernce:'+expence);
