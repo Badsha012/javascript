186bf4e6c56a56432db15d34852aaeda9786a96e
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-22 09:38:35 +0600

    Add files via upload

---
+++ basic5.js
@@ -0,0 +1,10 @@
+var a=prompt("Enter a number:");
+var b=parseFloat(prompt("Enter another number:"));
+var area=a*b;
+var tringle_area=0.5*a*b;
+
+//console.log("The area of the rectangle is "+area);
+document.write("The area of the rectangle is "+area);
+document.write("the triangle area="+tringle_area);
+
+
